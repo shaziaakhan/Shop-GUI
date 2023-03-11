cfec2ef15d4e03bc8bbd667a158a10c3ec5237a8
shaziaakhan <shazia.takhan@gmail.com>
2023-03-11 23:12:17 +0530

    new commit

--- prac_3.java
+++ prac_3.java
@@ -13,7 +13,8 @@ public class prac_3 extends JFrame implements ListSelectionListener,ActionListen
 {
 	JPanel p1,p2;
 	JList j1,j2;
-	JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16; 
+	JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18; 
+	JLabel t1,t2,t3,t4,t5,t6;
 	JButton b1,b2,b3;
 	int spen=100,spencil=150,spaper=150,seraser=75,ssharpener=75,sruler=120;
 	int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0;
@@ -206,12 +207,62 @@ public class prac_3 extends JFrame implements ListSelectionListener,ActionListen
 				l16.setBounds(210, 265, 50, 50);
 				l16.setFont(new Font("Serif", Font.PLAIN,16));
 				p2.add(l16);
+
+				t1=new JLabel();
+				t1.setText(Float.toString((float) (c1*2.99)));
+				t1.setBounds(410, 158, 50, 50);
+				t1.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t1);
+
+				t2=new JLabel();
+				t2.setText(Float.toString((float) (c2*9.99)));
+				t2.setBounds(410, 180, 50, 50);
+				t2.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t2);
+
+				t3=new JLabel();
+				t3.setText(Float.toString((float) (c3*4.99)));
+				t3.setBounds(410, 200, 50, 50);
+				t3.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t3);
+
+				t4=new JLabel();
+				t4.setText(Float.toString((float) (c4*2.99)));
+				t4.setBounds(410, 222, 50, 50);
+				t4.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t4);
+
+				t5=new JLabel();
+				t5.setText(Float.toString((float) (c5*3.99)));
+				t5.setBounds(410, 243, 50, 50);
+				t5.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t5);
+
+				t6=new JLabel();
+				t6.setText(Float.toString((float) (c6*14.99)));
+				t6.setBounds(410, 265, 50, 50);
+				t6.setFont(new Font("Serif", Font.PLAIN,16));
+				p2.add(t6);
+
+				l16=new JLabel("=========================================");
+		 		l16.setBounds(10, 300, 500, 50);
+		 		l16.setFont(new Font("Serif", Font.PLAIN,20));
+		 		p2.add(l16);
+
+				
 		     }
 		     else
 		     {
 		    	 l1.setText("Price of item:");
 		    	 l2.setText("Total cost: ");
 		    	 l3.setText("Available Stock:");
+				 p2.setVisible(false);
+				 l11.setText("");
+				 l12.setText("");
+				 l13.setText("");
+				 l14.setText("");
+				 l15.setText("");
+				 l16.setText("");
 		     }
 	      }
 	   @Override
